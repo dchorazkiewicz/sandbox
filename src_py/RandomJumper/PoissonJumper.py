8be22097dbb272da9fd8a5afb62c117b941c7d97
deymos314 <damian.ch.82@gmail.com>
2020-05-15 21:42:48 +0200

    .

--- src_py/RandomJumper/PoissonJumper.py
+++ src_py/RandomJumper/PoissonJumper.py
@@ -4,27 +4,30 @@ from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 
 class PoissonJumper(object):
-    def __init__(self, f_lambda, t, x):
+    def __init__(self, f_lambda, t0, x0):
         self.f_lambda = f_lambda
-        self.position = np.array([[t,x]])
-        
+        self.position = np.array([[t0,x0]])
+
+    def __str__(self):
+        return "PoissonJumper"
+
     def __call__(self,t,x):
         return self.f_lambda(t,x)
-    
+
     def get_last_position(self):
         return self.position[-1]
-        
+
     def get_homo_jump(self):
        return np.random.exponential(1)
-   
+
     def get_integral(self, t, t0, x0):
         return quad(lambda z: self.f_lambda(z, x0), t0, t)[0]
-    
+
     def get_inverse(self, val, tt0, xx0):
         return root_scalar(lambda z: self.get_integral(tt0+z,tt0,xx0)-val,
                            x0=0.9, fprime = lambda z: self.f_lambda(z,xx0),
                            bracket=[0,100], method='newton').root
-    
+
     def jump(self):
         HJ = self.get_homo_jump()
         # print(f'Homo jump= {HJ}')
@@ -36,21 +39,19 @@ class PoissonJumper(object):
         self.position=np.vstack((self.position, np.array([tt+NHJ, xx])))
         return 0
 
-        
+
 if __name__ == "__main__":
     print("..:: Test PoissonJumper ::..")
-    
-    PJ = PoissonJumper(lambda t, x: np.exp(-t/1000)+.01, 0., 10.)
+
+    PJ = PoissonJumper(lambda t, x: t/(10+t)+1, 0., 10.)
     print(PJ.position[-1])
     print(PJ(1,1))
     print(f'Homo jump: {PJ.get_homo_jump()}')
     print(f'Example integral: {PJ.get_integral(t=10,t0=2,x0=1)}')
     print(20*'~')
     print(PJ.get_inverse(8,0,0))
-    for i in range(1000):
+    for i in range(10):
         PJ.jump()
     x=PJ.position[:,0]
     y=(x[1:]-x[:-1])
-    plt.hist(y, bins=100)
-    plt.show()
-    
+    plt.scatter(np.arange(len(y)),np.cumsum(y))
